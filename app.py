353f4a3904a3754cf571db238b2723f0858bcd1a
YaXioz <alfianzyh@gmail.com>
2024-10-26 09:04:29 +0700

    route sederhana

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-import flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response
 
 app = Flask(__name__)
 
+@app.route("/", methods=['GET'])
+def root():
+    return "Hello World"
