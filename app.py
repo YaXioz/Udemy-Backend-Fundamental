b9c9b8dab33026572a4adb7e1dcfb5bcd88d3836
YaXioz <alfianzyh@gmail.com>
2024-10-26 09:28:27 +0700

    update file

--- app.py
+++ app.py
@@ -1,14 +1,1 @@
-from flask import Flask, request, jsonify, make_response
-
-app = Flask(__name__)
-
-@app.route("/", methods=['GET'])
-def root():
-    return "Hello World"
-
-@app.route("/about", methods=['GET'])
-def about():
-    return make_response(jsonify({"nama": "Alfian", "umur": "20"}))
-
-if __name__ == "__main__" :
-    app.run()
+from flask import Flask, request, jsonify
