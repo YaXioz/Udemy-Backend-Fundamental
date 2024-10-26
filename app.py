4535fcc9434f1524eee1bdd86b32ae1140f30d19
YaXioz <alfianzyh@gmail.com>
2024-10-26 09:14:38 +0700

    test route on browser

--- app.py
+++ app.py
@@ -5,3 +5,10 @@ app = Flask(__name__)
 @app.route("/", methods=['GET'])
 def root():
     return "Hello World"
+
+@app.route("/about", methods=['GET'])
+def about():
+    return make_response(jsonify({"nama": "Alfian", "umur": "20"}))
+
+if __name__ == "__main__" :
+    app.run()
